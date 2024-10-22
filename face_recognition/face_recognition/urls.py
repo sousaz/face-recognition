3bf964018e1ca8fa83cbb048dffc03fd0bff8e7c
sousaz <gabrielsousa2003gsa@gmail.com>
2024-10-22 19:55:47 -0300

    Criando a estrutura de pastas

--- face_recognition/face_recognition/urls.py
+++ face_recognition/face_recognition/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from event_control.views import teste
+from event_control.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
