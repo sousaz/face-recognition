6fc8b132679e0489da8b245bad3242b25bfeef3f
sousaz <gabrielsousa2003gsa@gmail.com>
2024-11-23 21:21:07 -0300

    criando os grupos juntos com o migrate

--- projeto/event_control/apps.py
+++ projeto/event_control/apps.py
@@ -1,6 +1,29 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class EventControlConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'event_control'
+
+    def ready(self):
+        post_migrate.connect(create_default_groups, sender=self)
+
+def create_default_groups(sender, **kwargs):
+    from django.contrib.auth.models import Group, Permission
+    
+    groups = {
+        'admin': "__all__",
+        'student': ["view_student"]
+    }
+
+    for group_name, permissions in groups.items():
+        group, create = Group.objects.get_or_create(name=group_name)
+
+        if permissions == "__all__":
+            all_permissions = Permission.objects.all()
+            group.permissions.set(all_permissions)
+        elif permissions:
+            for codename in permissions:
+                permission = Permission.objects.filter(codename=codename).first()
+                if permission:
+                    group.permissions.add(permission)
